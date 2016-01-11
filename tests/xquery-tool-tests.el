95d0c4e384d6d2a3019122600393bb880e3fcf46
Paddy at home <pma@rdorte.org>
2016-01-11 12:22:28 +0100

    better check if argument is actually a file.

--- tests/xquery-tool-tests.el
+++ tests/xquery-tool-tests.el
@@ -32,7 +32,8 @@ Does not check the links, though."
 <price xmlns:tmplink=\"potemkin\" tmplink:start=\"%p#302\">$7.95</price>
 <price xmlns:tmplink=\"potemkin\" tmplink:start=\"%p#507\">$8.95</price>
 <price xmlns:tmplink=\"potemkin\" tmplink:start=\"%p#715\">$4.50</price>
-<price xmlns:tmplink=\"potemkin\" tmplink:start=\"%p#898\">$6.95</price>" (format-spec-make ?p (buffer-file-name tmp)))))))
+<price xmlns:tmplink=\"potemkin\" tmplink:start=\"%p#898\">$6.95</price>" (format-spec-make ?p (buffer-file-name tmp))))
+	   ("/" nil nil nil "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<breakfast_menu>\n	  <food>\n		    <name>Belgian Waffles</name>\n		    <price>$5.95</price>\n		    <description>Two of our famous Belgian Waffles with plenty of real maple syrup</description>\n		    <calories>650</calories>\n	  </food>\n	  <food>\n		    <name>Strawberry Belgian Waffles</name>\n		    <price>$7.95</price>\n		    <description>Light Belgian waffles covered with strawberries and whipped cream</description>\n		    <calories>900</calories>\n	  </food>\n	  <food>\n		    <name>Berry-Berry Belgian Waffles</name>\n		    <price>$8.95</price>\n		    <description>Light Belgian waffles covered with an assortment of fresh berries and whipped cream</description>\n		    <calories>900</calories>\n	  </food>\n	  <food>\n		    <name>French Toast</name>\n		    <price>$4.50</price>\n		    <description>Thick slices made from our homemade sourdough bread</description>\n		    <calories>600</calories>\n	  </food>\n	  <food>\n		    <name>Homestyle Breakfast</name>\n		    <price>$6.95</price>\n		    <description>Two eggs, bacon or sausage, toast, and our ever-popular hash browns</description>\n		    <calories>950</calories>\n	  </food>\n</breakfast_menu>"))))
     (dolist (case cases)
       (with-current-buffer tmp
 	(erase-buffer)
